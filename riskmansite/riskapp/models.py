1d964e30d02827daa150802fadbf9109cb7bc455
Octavio <octaviomdq93@gmail.com>
2018-08-13 16:13:32 -0300

    Models, and initial testing.

--- riskmansite/riskapp/models.py
+++ riskmansite/riskapp/models.py
@@ -1,10 +1,38 @@
+import datetime
+
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+
 # Create your models here.
 
 
+class Insurers(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.id
+
+
 class RiskTypes(models.Model):
-    id = models.AutoField
-    code = models.CharField(max_length=4)
-    full_risk_name = models.CharField
-    risk_metadata = JSONField
+    code = models.CharField(max_length=4, primary_key=True)
+    full_risk_name = models.CharField(max_length=100)
+    risk_metadata = JSONField()
+
+    def __str__(self):
+        return [self.code, self.full_risk_name, self.risk_metadata]
+
+
+class RiskEntries(models.Model):
+    id = models.AutoField(primary_key=True)
+    insurer_id = models.ForeignKey(Insurers, on_delete=models.CASCADE)
+    code = models.ForeignKey(RiskTypes, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.date.today)
+    risk_data = JSONField()
+
+    class Meta:
+        unique_together=("id","code") # workaround to composite key
+
+    def __str__(self):
+        return [self.id, self.code, self.date, self.risk_data]
--- riskmansite/riskapp/tests.py
+++ riskmansite/riskapp/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import *
+
+import json
+
 
 # Create your tests here.
+
+risktype_json = {'name': 'text', 'date': 'date'}
+riskentry_json = {'name': 'Richard Test', 'date': '08-07-2018'}
+
+class RisksTestCase(TestCase):
+
+    def setUp(self):
+        insurer = Insurers(first_name='Test', last_name='Insurer')
+        risk_type = RiskTypes(code='TEST', full_risk_name='This is a test entry', risk_metadata=json.dumps(risktype_json))
+        insurer.save()
+        risk_type.save()
+        risk_entry = RiskEntries(code=risk_type, insurer_id=insurer, risk_data=json.dumps(riskentry_json))
+        risk_entry.save()
+
+    def test_correct_RiskTypes_save(self):
+        q = RiskTypes.objects.get(code='TEST')
+        self.assertEquals(q.code,'TEST')
+        json_qdata = json.loads(q.risk_metadata)
+        self.assertEquals(risktype_json['name'],json_qdata['name'])
+
+    def test_correct_RiskEntries_save(self):
+        q = RiskEntries.objects.get(code='TEST')
+        json_qdata = json.loads(q.risk_data)
+        self.assertEquals(riskentry_json['name'],json_qdata['name'])
+        self.assertEquals(q.insurer_id.id,1)
